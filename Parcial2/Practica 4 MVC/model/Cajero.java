38a091abf7cd3705c583eb3e1724fda8f1729aae
VALENTE CUEVAS JUAN CARLOS <22030371@itcelaya.edu.mx>
2025-03-04 13:47:02 -0600

    Practica 4 MVC

---
+++ Parcial2/Practica 4 MVC/model/Cajero.java
@@ -0,0 +1,58 @@
+package model;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Cajero<T extends Number & Comparable<T>> {
+    private final List<T> denominaciones;
+    private final List<T> stock;
+    private T cantidad; // monto a cambiar
+
+    public Cajero(List<T> denominaciones, List<T> stock) {
+        this.denominaciones = denominaciones;
+        this.stock = stock;
+    }
+
+    public void setCantidad(T cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public List<Integer> darCambio() throws Exception {
+        int restante = cantidad.intValue();
+        List<Integer> resultado = new ArrayList<>();
+        for (int i = 0; i < denominaciones.size(); i++) {
+            resultado.add(0);
+        }
+
+        while (restante > 0) {
+            boolean usadoAlgunaVez = false;
+
+            for (int i = 6; i < denominaciones.size(); i++) {
+                int den = denominaciones.get(i).intValue();
+                int stk = stock.get(i).intValue();
+                if (den <= restante && stk > 0) {
+                    resultado.set(i, resultado.get(i) + 1);
+                    stock.set(i, (T) Integer.valueOf(stk - 1));
+                    restante -= den;
+                    usadoAlgunaVez = true;
+                    if (restante == 0) break;
+                }
+            }
+            if (restante == 0) break;
+            for (int i = 0; i < 6 && restante > 0; i++) {
+                int den = denominaciones.get(i).intValue();
+                int stk = stock.get(i).intValue();
+                if (den <= restante && stk > 0) {
+                    resultado.set(i, resultado.get(i) + 1);
+                    stock.set(i, (T) Integer.valueOf(stk - 1));
+                    restante -= den;
+                    usadoAlgunaVez = true;
+                    if (restante == 0) break;
+                }
+            }
+            if (!usadoAlgunaVez) {
+                throw new Exception("No se puede dar el cambio exacto con el Stock Actual.");
+            }
+        }
+        return resultado;
+    }
+}
